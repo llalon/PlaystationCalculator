0d9dcae12efa2651025c5b27502594be296cfc13
Liam Lalonde <71791017+llalon@users.noreply.github.com>
2021-03-06 15:38:46 -0500

    Dev (#2)
    * Started
    
    * Added Parse Function
    
    * Added stuff
    
    * Added df of prices
    
    * Added warning
    
    * Ran analysis
    
    * Added PS4
    
    * Fixed Analysis

--- main.py
+++ main.py
@@ -9,6 +9,35 @@ import os
 import pandas as pd
 import numpy as np
 
+# Function to remove price outliers
+# https://stackoverflow.com/questions/11686720/is-there-a-numpy-builtin-to-reject-outliers-from-a-list
+def reject_outliers(data, m=2):
+    return data[abs(data - np.mean(data)) < m * np.std(data)]
+
+
+# Returns a list of price combinations
+def find_combns(levels=[1, 2, 3, 4], target=5):
+
+    combn = []
+
+    # Find all the combinations
+    # Adapted from: https://stackoverflow.com/questions/34517540/find-all-combinations-of-a-list-of-numbers-with-a-given-sum
+    def subset_sum(numbers, target, partial=[]):
+        s = sum(partial)
+
+        # check if the partial sum is equals to target
+        if s == target:
+            combn.append(partial)
+        if s >= target:
+            return  # if we reach the number why bother to continue
+
+        for i in range(len(numbers)):
+            n = numbers[i]
+            subset_sum(numbers, target, partial + [n])
+
+    subset_sum(levels, target)
+
+    return combn
 
 def parse(item):
     # Adapted from https://www.blog.datahut.co/post/scraping-ebay
@@ -53,11 +82,10 @@ def parse(item):
 
     return df
 
-
-def build_price_df(file_name):
+def build_price_df(file_name="ps_prices.csv"):
 
     # Find average prices of each level of playstation
-    playstations = [1, 2, 3]
+    playstations = [1, 2, 3, 4]
 
     df_playstations = pd.DataFrame()
 
@@ -68,6 +96,7 @@ def build_price_df(file_name):
 
     # Save to file
     df_playstations.to_csv(file_name)
+    return df_playstations
 
 
 def main():
@@ -77,7 +106,12 @@ def main():
     # Check if price data is present
     if not os.path.exists(FILE_NAME):
         print("WARNING: PS price data not found. Building index...")
-        build_price_df(FILE_NAME)
+        df = build_price_df(FILE_NAME)
+    else:
+        # Import price data
+        df = pd.read_csv("ps_prices.csv")
+
+    df = df.dropna()
 
 
 if __name__ == "__main__":
