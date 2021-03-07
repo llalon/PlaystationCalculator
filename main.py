268b465a5620a6e91387f7d9914450e5d6b0fbc7
llalon <71791017+llalon@users.noreply.github.com>
2021-03-07 11:09:17 -0500

    Moved Functions

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ def main():
 
     FILE_NAME = "ps_prices.csv"
 
+    force_refresh = False
+
     # Check if price data is present
-    if not os.path.exists(FILE_NAME):
+    if not os.path.exists(FILE_NAME) or force_refresh:
         print("WARNING: PS price data not found. Building index...")
         df = ps_parse.build_price_df(FILE_NAME)
     else:
-        # Import price data
-        df = pd.read_csv("ps_prices.csv")
+        print("Loading data from disk...")
+        df = pd.read_csv(FILE_NAME)
+
+    # Run analysis
+    ps_calc.run(df)
 
 
 if __name__ == "__main__":
--- ps_calc.py
+++ ps_calc.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
 
-# Function to remove price outliers
 # https://stackoverflow.com/questions/11686720/is-there-a-numpy-builtin-to-reject-outliers-from-a-list
 def reject_outliers(data, m=2):
     return data[abs(data - np.mean(data)) < m * np.std(data)]
 
 
 # Returns a list of price combinations
-def find_combns(levels=[1, 2, 3, 4], target=5):
+def find_combos(levels=[1, 2, 3, 4], target=5):
 
     combn = []
 
@@ -30,3 +29,29 @@ def find_combns(levels=[1, 2, 3, 4], target=5):
     subset_sum(levels, target)
 
     return combn
+
+
+def run(df):
+
+    # Add PS levels as column
+    df["levels"] = df.apply(lambda x: int(x["item"].split(" ")[1]), axis=1)
+
+    # Find average price per level with outliers removed.
+    levels = df.levels.unique()
+    avg_price = []
+    for l in levels:
+        dfl = df.loc[df["levels"] == l]
+        prices = dfl["price"].to_numpy()
+        avg = np.mean(reject_outliers(prices))
+        avg_price.append(avg)
+
+    # Calculate best price combinations
+    prices_summary = []
+    for c in find_combos():
+        p = 0
+        for i in c:
+            p = p + avg_price[i - 1]
+
+        prices_summary.append({"combo": c, "price": p})
+
+    print(prices_summary)
